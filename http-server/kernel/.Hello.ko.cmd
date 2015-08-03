360fa3e404224563fc24ad050f4c03f1f31912d8
ywang <Yaowang.future@gmail.com>
2015-08-03 14:32:00 +0800

    hello burden

---
+++ http-server/kernel/.Hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/wangyao/Documents/unique-task/http-server/kernel/Hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/wangyao/Documents/unique-task/http-server/kernel/Hello.ko /home/wangyao/Documents/unique-task/http-server/kernel/Hello.o /home/wangyao/Documents/unique-task/http-server/kernel/Hello.mod.o
---
+++ http-server/kernel/.tmp_versions/Hello.mod
@@ -0,0 +1,2 @@
+/home/wangyao/Documents/unique-task/http-server/kernel/Hello.ko
+/home/wangyao/Documents/unique-task/http-server/kernel/Hello.o
